2926711bcf0863651065db541c8523ad8a48b851
Thành An <thanhan1552006@gmail.com>
2026-06-18 07:49:41 +0700

    FE BE for Home

--- Cinema-Management/Data/ApplicationDbContext.cs
+++ Cinema-Management/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
+
 using Cinema_Management.Models;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Cinema_Management.Data;
 
@@ -11,4 +13,16 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<User> Users { get; set; }
+    public DbSet<MovieViewModel> Movies { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<MovieGenre> MovieGenres { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Khai báo khóa chính kép cho bảng trung gian
+        modelBuilder.Entity<MovieGenre>()
+            .HasKey(mg => new { mg.MovieID, mg.GenreID });
+    }
 }
